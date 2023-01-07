81ba2afdd93c0cfaddf9d6d9a0eb31979a34053d
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-01-07 10:06:25 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const repo = "memory-game"
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
     reactStrictMode: true,
     experimental: {
         appDir: true,
     },
+    assetPrefix: assetPrefix,
+    basePath: basePath
 };
 
 module.exports = nextConfig;
